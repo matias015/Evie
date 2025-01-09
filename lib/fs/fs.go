2182a2293aaf933e9a7d4adf16ab63a1475ff175
matias015 <matiasjf015@gmail.com>
2025-01-09 14:58:05 -0300

    09/01/25

--- lib/fs/fs.go
+++ lib/fs/fs.go
@@ -1,111 +1,172 @@
-package lexer
+package fsLib
 
-type Token struct {
-	Kind   TokenType
-	Lexeme string
-	Line   int
-	Column int
-}
-
-type TokenType int
-
-const (
-	TOKEN_IDENTIFIER = iota
-
-	TOKEN_VAR
-	TOKEN_IMPORT
-	TOKEN_LOOP
-	TOKEN_FN
-	TOKEN_IF
-	TOKEN_NOTHING
-	TOKEN_AS
-	TOKEN_ELSE
-	TOKEN_ELSEIF
-	TOKEN_STRUCT
-	TOKEN_FOR
-	TOKEN_CONTINUE
-	TOKEN_BREAK
-	TOKEN_IN
-	TOKEN_OR
-	TOKEN_AND
-	TOKEN_NOT
-	TOKEN_TRUE
-	TOKEN_FALSE
-	TOKEN_RETURN
-	TOKEN_TRY
-	TOKEN_CATCH
-	TOKEN_FINALLY
-
-	TOKEN_NUMBER
-	TOKEN_STRING
-	TOKEN_BOOLEAN
-	TOKEN_LBRACKET
-	TOKEN_RBRACKET
-	TOKEN_RBRACE
-	TOKEN_LBRACE
-	TOKEN_LPAR
-	TOKEN_RPAR
-	TOKEN_COMMA
-	TOKEN_COLON
-	TOKEN_TERNARY
-	TOKEN_DOT
-	TOKEN_LARROW
-	TOKEN_OPERATOR
-	TOKEN_ASSIGN
-	TOKEN_EOF
-	TOKEN_EOL
-	TOKEN_INIT
+import (
+	"bufio"
+	environment "evie/env"
+	"evie/values"
+	"log"
+	"os"
 )
 
-var tokenTypeLookUp = map[TokenType]string{
-	TOKEN_VAR:      "var",
-	TOKEN_IMPORT:   "import",
-	TOKEN_LOOP:     "loop",
-	TOKEN_FN:       "fn",
-	TOKEN_IF:       "if",
-	TOKEN_NOTHING:  "Nothing",
-	TOKEN_AS:       "as",
-	TOKEN_ELSE:     "else",
-	TOKEN_ELSEIF:   "elseif",
-	TOKEN_STRUCT:   "struct",
-	TOKEN_FOR:      "for",
-	TOKEN_CONTINUE: "continue",
-	TOKEN_BREAK:    "break",
-	TOKEN_IN:       "in",
-	TOKEN_OR:       "or",
-	TOKEN_AND:      "and",
-	TOKEN_NOT:      "not",
-	TOKEN_TRUE:     "true",
-	TOKEN_FALSE:    "false",
-	TOKEN_RETURN:   "return",
-	TOKEN_TRY:      "try",
-	TOKEN_CATCH:    "catch",
-	TOKEN_FINALLY:  "finally",
-	TOKEN_NUMBER:   "number",
-	TOKEN_STRING:   "string",
-	TOKEN_BOOLEAN:  "boolean",
-	TOKEN_LBRACKET: "[",
-	TOKEN_RBRACKET: "]",
-	TOKEN_RBRACE:   "}",
-	TOKEN_LBRACE:   "{",
-	TOKEN_LPAR:     "(",
-	TOKEN_RPAR:     ")",
-	TOKEN_COMMA:    ",",
-	TOKEN_COLON:    ":",
-	TOKEN_TERNARY:  "?",
-	TOKEN_DOT:      ".",
-	TOKEN_LARROW:   "->",
-	TOKEN_OPERATOR: "operator",
-	TOKEN_ASSIGN:   "=",
-	TOKEN_EOF:      "eof",
-	TOKEN_EOL:      "eol",
-	TOKEN_INIT:     "init",
+func GetFileStruct() values.StructValue {
+	fileStruct := values.StructValue{}
+	fileStruct.Properties = []string{"descriptor"}
+	fileStruct.Methods = make(map[string]values.RuntimeValue)
+	return fileStruct
 }
 
-func GetTokenName(tokenType TokenType) string {
-	return tokenTypeLookUp[tokenType]
+func Load(env *environment.Environment) {
+
+	ns := values.NamespaceValue{}
+
+	methods := make(map[string]values.RuntimeValue, 4)
+
+	methods["read"] = values.NativeFunctionValue{Value: ReadFile}
+	methods["open"] = values.NativeFunctionValue{Value: OpenFile}
+	methods["exists"] = values.NativeFunctionValue{Value: FileExists}
+	methods["remove"] = values.NativeFunctionValue{Value: RemoveFile}
+	methods["create"] = values.NativeFunctionValue{Value: CreateFile}
+
+	methods["createDir"] = values.NativeFunctionValue{Value: CreateDir}
+	methods["existsDir"] = values.NativeFunctionValue{Value: FileExistsDir}
+	methods["removeDir"] = values.NativeFunctionValue{Value: RemoveDir}
+	methods["moveOrRenameDir"] = values.NativeFunctionValue{Value: MoveDir}
+	ns.Value = methods
+
+	env.ForceDeclare("fs", ns)
+
 }
 
-func (tokenType TokenType) String() string {
-	return tokenTypeLookUp[tokenType]
+func ReadFile(args []values.RuntimeValue) values.RuntimeValue {
+	datosComoBytes, err := os.ReadFile(args[0].(values.StringValue).Value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convertir el arreglo a string
+	datosComoString := string(datosComoBytes)
+	// imprimir el string
+	return values.StringValue{Value: datosComoString}
+}
+
+func OpenFile(args []values.RuntimeValue) values.RuntimeValue {
+
+	file, err := os.OpenFile(args[0].(values.StringValue).Value, os.O_RDWR|os.O_APPEND, 0644)
+
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+
+	f := FileValue{}
+
+	f.Value = file
+	f.Scanner = bufio.NewScanner(file)
+	f.Closed = false
+
+	return f
+}
+
+func FileExists(args []values.RuntimeValue) values.RuntimeValue {
+	arg := args[0]
+	_, err := os.Stat(arg.(values.StringValue).Value)
+	if err != nil {
+		return values.BoolValue{Value: false}
+	}
+	return values.BoolValue{Value: true}
+}
+
+func RemoveFile(args []values.RuntimeValue) values.RuntimeValue {
+	err := os.Remove(args[0].(values.StringValue).Value)
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+	return values.BoolValue{Value: true}
+}
+
+func CreateFile(args []values.RuntimeValue) values.RuntimeValue {
+
+	if len(args) == 0 {
+		return values.ErrorValue{Value: "Missing arguments for file create method"}
+	}
+
+	if args[0].GetType() != values.StringType {
+		return values.ErrorValue{Value: "First argument for file create method must be a string"}
+	}
+
+	file, err := os.Create(args[0].(values.StringValue).Value)
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+	return &FileValue{Value: file, Closed: false, Scanner: bufio.NewScanner(file)}
+}
+
+// Directory
+
+func CreateDir(args []values.RuntimeValue) values.RuntimeValue {
+
+	if len(args) == 0 {
+		return values.ErrorValue{Value: "Missing arguments for directory create method"}
+	}
+
+	if args[0].GetType() != values.StringType {
+		return values.ErrorValue{Value: "First argument for directory create method must be a string"}
+	}
+
+	err := os.Mkdir(args[0].(values.StringValue).Value, 0755)
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+	return values.BoolValue{Value: true}
+}
+
+func RemoveDir(args []values.RuntimeValue) values.RuntimeValue {
+
+	if len(args) == 0 {
+		return values.ErrorValue{Value: "Missing arguments for directory remove method"}
+	}
+
+	if args[0].GetType() != values.StringType {
+		return values.ErrorValue{Value: "First argument for directory remove method must be a string"}
+	}
+
+	err := os.RemoveAll(args[0].(values.StringValue).Value)
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+	return values.BoolValue{Value: true}
+}
+
+func FileExistsDir(args []values.RuntimeValue) values.RuntimeValue {
+
+	if len(args) == 0 {
+		return values.ErrorValue{Value: "Missing arguments for directory exists method"}
+	}
+
+	if args[0].GetType() != values.StringType {
+		return values.ErrorValue{Value: "First argument for directory exists method must be a string"}
+	}
+
+	_, err := os.Stat(args[0].(values.StringValue).Value)
+	if err != nil {
+		return values.BoolValue{Value: false}
+	}
+
+	return values.BoolValue{Value: true}
+}
+
+func MoveDir(args []values.RuntimeValue) values.RuntimeValue {
+
+	if len(args) < 1 {
+		return values.ErrorValue{Value: "Missing arguments for directory move method"}
+	}
+
+	if args[0].GetType() != values.StringType || args[1].GetType() != values.StringType {
+		return values.ErrorValue{Value: "Arguments for directory move method must be a string"}
+	}
+
+	err := os.Rename(args[0].(values.StringValue).Value, args[1].(values.StringValue).Value)
+	if err != nil {
+		return values.ErrorValue{Value: err.Error()}
+	}
+	return values.BoolValue{Value: true}
+
 }
